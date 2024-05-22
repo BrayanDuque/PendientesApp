e87078ccdcd8acc0d0544abbb9c1e26c218b7f5a
Brayan Duque <bradumar2016@gmail.com>
2024-05-22 11:43:56 -0500

    se añade la nueva sintax de @if

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
 import {InicioComponent} from './pages/inicio/inicio.component'
-import {PruebasComponent} from './pages/pruebas/pruebas.component'
+
 export const routes: Routes = [
     {
         path: '',
         component: InicioComponent
-    },
-    {
-        path: 'pruebas',
-        component: PruebasComponent
     }
-];
+] 
